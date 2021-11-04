279d98e5c6462a6756b3e5faa73f493a58558675
Manjunath_Hanmantgad <hanamanthgad123@gmail.com>
2021-11-04 10:56:18 +0530

    Add files via upload

---
+++ Azure Machine learning/Azure databricks/code/Incremental load/configuration.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+raw_folder_path = "/mnt/adlstorage01/raw"
+processed_folder_path = "/mnt/adlstorage01/processed"
+presentation_folder_path = "/mnt/adlstorage01/presentation"
+
+# COMMAND ----------
+
+
