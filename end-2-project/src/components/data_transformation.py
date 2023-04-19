55dc4cd4612ce52f1aa54bbf98b0b799dad60c9b
Manjunath <hanamanthgad123@gmail.com>
2023-04-19 08:19:15 +0530

    coding

--- end-2-project/src/components/data_transformation.py
+++ end-2-project/src/components/data_transformation.py
@@ -1,1 +1,16 @@
 # contains code related to feature engineering.
+# do feature engineering
+# deal with categorical features
+# missing values
+import os 
+import sys 
+from dataclasses import dataclass
+import numpy as np
+import pandas as pd 
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+# import custom exception and logger
+from src.exception import CustomException
+from src.logger import logging
