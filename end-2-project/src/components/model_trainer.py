aeb7d91a1d1beff65d40d078cf6d819e90d32a9f
Manjunath <hanamanthgad123@gmail.com>
2023-04-19 10:28:07 +0530

    coding

--- end-2-project/src/components/model_trainer.py
+++ end-2-project/src/components/model_trainer.py
@@ -19,4 +19,38 @@ from src.exception import CustomException
 from src.logger import logging
 from src.utils import save_object,evaluate_models
 
+# create config file
+
 @dataclass
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join("artifacts", "model.pkl")
+    
+class ModelTrainer():
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+    def initiate_model_trainer(self,train_arr,test_arr,preprocessor_path):
+        try:
+            logging.info("Beginning the model training")
+            logging.info("Splitting data into train and test")
+            X_train,y_train,X_test,y_test = (
+                train_arr[:,:-1],  # X-train# take out last column
+                train_arr[:,-1], # y_train
+                test_arr[:,:-1] , # X_test
+                test_arr[:,-1] # y_test
+            )
+            # define the models here
+            
+            models = {
+                "Random Forest": RandomForestRegressor(),
+                "Decision tree": DecisionTreeRegressor(),
+                "Linear regression": LinearRegression(),
+                "K neighbours regression": KNeighborsRegressor(),
+                "XGBRegressor": XGBRegressor(),
+                "Adaboost Regressor": AdaBoostRegressor()
+            }
+            model_report: dict= evaluate_model(X_train,y_train,models=models)
+            
+        except:
+            pass
+        
+            
--- end-2-project/src/utils.py
+++ end-2-project/src/utils.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd 
 from src.exception import CustomException
 import dill # used for creating pickle
+from sklearn.metrics import r2_score
 
 def save_object(file_path,obj):
     '''function that we will use in data transformation to save object'''
@@ -14,3 +15,23 @@ def save_object(file_path,obj):
             dill.dump(obj, file_obj)
     except Exception as e:
         return CustomException(e,sys)
+    
+def evaluate_models(X_train,y_train,X_test,y_test, models):
+    try:
+        
+        '''used in model trainer file'''
+        
+        report = {}
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            model.fit(X_train,y_train)
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+            train_model_score = r2_score(y_train,y_train_pred)
+            test_model_score = r2_score(y_test,y_test_pred)
+            report[list(models.keys()[i])] = test_model_score
+        return report
+    except Exception as e:
+        return CustomException(e,sys)
+
+        
