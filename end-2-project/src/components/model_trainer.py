b783dfd8c13d381490fee2b147f26bc245d3a65a
Manjunath <hanamanthgad123@gmail.com>
2023-04-19 10:37:14 +0530

    coding

--- end-2-project/src/components/model_trainer.py
+++ end-2-project/src/components/model_trainer.py
@@ -69,6 +69,10 @@ class ModelTrainer():
                 file_path = self.model_trainer_config.trained_model_file_path,
                 obj=best_model
             )
+            
+            predicted = best_model.predict(X_test)
+            r2_square = r2_score(y_test, predicted)
+            return r2_square
         except Exception as e:
             return CustomException(e,sys)
         
