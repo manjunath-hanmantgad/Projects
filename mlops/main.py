41eb8514c51b8105ecedd369eed1d6b558147ed3
Manjunath <hanamanthgad123@gmail.com>
2024-01-09 07:59:36 +0530

    preprocessing in mlops

--- mlops/main.py
+++ mlops/main.py
@@ -4,6 +4,7 @@ from src.database.db_connection import create_db_engine
 from src.database.query_builder import build_select_all_query
 from src.extraction.data_extraction import extract_data_from_db
 from src.preprocessing.data_preprocessing import handle_missing_values, handle_categorical_variables, feature_scaling, handle_outliers, data_splitting
+from src.feature_engineering.feature_engineering import create_interaction_terms, select_best_features
 
 def main():
     # Connect to the database and retrieve data
@@ -18,9 +19,14 @@ def main():
     data = feature_scaling(data)
     data = handle_outliers(data)
 
+    # Feature Engineering
+    data = create_interaction_terms(data, interaction_degree=2)
+    data = select_best_features(data, k=4)
+
     # Data Splitting
     train_data, test_data = data_splitting(data)
 
+    # Now you can proceed with training and evaluating your machine learning model using train_data and test_data
 
 if __name__ == "__main__":
     main()
